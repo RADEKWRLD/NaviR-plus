44506d3a23bd660fa76700f52753183dcaa617d0
RADEKWRLD <1691357101@qq.com>
2025-12-29 19:30:31 +0800

    feat: 添加 @vercel/speed-insights 依赖并在布局中集成速度分析组件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Providers from "@/components/Providers";
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={oxanium.variable}>
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
